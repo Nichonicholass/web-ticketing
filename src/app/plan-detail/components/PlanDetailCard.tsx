683ba9837b55db2e19002033285d4d03c1225705
MadeSatya5 <madesatya505@gmail.com>
2025-05-16 21:46:37 +0700

    fix: plandetailcard component

--- src/app/plan-detail/components/PlanDetailCard.tsx
+++ src/app/plan-detail/components/PlanDetailCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Button from "@/components/buttons/Button";
 import ConfirmModal from "@/components/modal/ConfirmModal";
 import Typography from "@/components/Typography";
 import {
@@ -8,6 +9,7 @@ import {
   CardFooter,
   CardHeader,
 } from "@/components/ui/card";
+import { Plus } from "lucide-react";
 import { useState } from "react";
 import { RxCross2 } from "react-icons/rx";
 
@@ -16,17 +18,29 @@ type PlanDetailCardProps = {
   kelas: string;
   dosen: string;
   date: string;
+  status: string;
 };
 
-function PlanDetailCard({ id, kelas, dosen, date }: PlanDetailCardProps) {
+function PlanDetailCard({
+  id,
+  kelas,
+  dosen,
+  date,
+  status,
+}: PlanDetailCardProps) {
   const [open, setOpen] = useState(false);
 
   return (
-    <Card className="w-[300px] md:w-[434px] text-black rounded-[6px] pb-7 relative hover:scale-105 transition-transform duration-300">
-      <RxCross2
-        className="text-black text-xl absolute top-4 right-3 cursor-pointer"
-        onClick={() => setOpen(true)}
-      />
+    <Card className="w-[300px] px-4 md:w-[434px] text-black rounded-[6px] pb-4 relative hover:scale-105 transition-transform duration-300">
+      {status === "on_plan" ? (
+        <RxCross2
+          className="text-black text-xl absolute top-4 right-3 cursor-pointer"
+          onClick={() => setOpen(true)}
+        />
+      ) : (
+        ""
+      )}
+
       <ConfirmModal
         open={open}
         setOpen={setOpen}
@@ -47,6 +61,18 @@ function PlanDetailCard({ id, kelas, dosen, date }: PlanDetailCardProps) {
       <CardFooter className="flex justify-between border border-black w-fit h-fit py-[4px] px-[12px] rounded-[4px] ml-6 ">
         <Typography variant="p">{date}</Typography>
       </CardFooter>
+
+      {status === "not_plan" ? (
+        <Button
+          variant="slate"
+          leftIcon={Plus}
+          leftIconClassName="h-5 w-5"
+          className="flex w-full h-fit items-center justify-center mt-6"
+          // onClick={() => setOpen(true)}
+        />
+      ) : (
+        ""
+      )}
     </Card>
   );
 }
