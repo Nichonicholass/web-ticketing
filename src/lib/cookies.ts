f98f0de880b6b9ff0a89f6c8f0feff455be17d6f
Nichonicholass <nicholassamuel34@gmail.com>
2025-05-16 23:38:19 +0700

    fix: fix cookie

--- src/lib/cookies.ts
+++ src/lib/cookies.ts
@@ -1,12 +1,27 @@
+// import Cookies from "universal-cookie";
+
+// const cookies = new Cookies();
+
+// export const getToken = (): string => cookies.get("@nexttemplate/token");
+
+// export const setToken = (token: string) => {
+//   cookies.set("@nexttemplate/token", token, { path: "/" });
+// };
+
+// export const removeToken = () =>
+//   cookies.remove("@nexttemplate/token", { path: "/" });
+
 import Cookies from "universal-cookie";
 
 const cookies = new Cookies();
+const TOKEN_KEY = "nexttemplate_token";
 
-export const getToken = (): string => cookies.get("@nexttemplate/token");
+export const getToken = (): string => cookies.get(TOKEN_KEY);
 
 export const setToken = (token: string) => {
-  cookies.set("@nexttemplate/token", token, { path: "/" });
+  cookies.set(TOKEN_KEY, token, { path: "/" });
 };
 
-export const removeToken = () =>
-  cookies.remove("@nexttemplate/token", { path: "/" });
+export const removeToken = () => {
+  cookies.remove(TOKEN_KEY, { path: "/" });
+};
